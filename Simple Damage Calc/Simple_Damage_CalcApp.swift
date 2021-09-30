6b73e9239b46794c91a312e1a6e3f0becadef811
JaceJoseph <mgrex98@gmail.com>
2021-09-30 11:01:12 +0700

    Initial Commit

---
+++ Simple Damage Calc/Simple_Damage_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Damage_CalcApp.swift
+//  Simple Damage Calc
+//
+//  Created by Jesse Joseph on 30/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Damage_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
